f863c4fb1f7c3287bb5eeeb06e7c2efbeb72b1fc
Victor Suarez <victorsuarez0507@gmail.com>
2023-06-07 00:44:05 -0500

    Se terminaron de configurar las funciones eliminarContacto y agregarContacto

--- listaContacto.js
+++ listaContacto.js
@@ -15,12 +15,15 @@ let contactos = [{
         direccion : "Calle 20"
 }
 ]
+
 let id = Number(prompt("Ingrese el id"));
 let nombres = prompt("Ingrese los nombres");
 let apellidos = prompt("Ingrese los apellidos");
 let telefono = prompt("Ingrese el telefono");
 let ciudad = prompt("Ingrese la ciudad");
 let direccion = prompt("Ingrese la direccion");
+let borrar = Number(prompt("Id del contacto a eliminar"));
+
 
 function NuevoContacto(id, nombres, apellidos, telefono, ciudad, direccion){
     this.id = id;
@@ -30,29 +33,50 @@ function NuevoContacto(id, nombres, apellidos, telefono, ciudad, direccion){
     this.ciudad = ciudad;
     this.direccion = direccion;
 }
-let contacto3 = new NuevoContacto(id, nombres, apellidos, telefono, ciudad, direccion)
+let contacto3 = new NuevoContacto(id, nombres, apellidos, telefono, ciudad, direccion);
+
 
 function agregarContacto(){ 
-    contactos.push(contacto3);      
-}
-
-let borrar = prompt("Id del contacto a eliminar")
-function eliminarContacto(){ 
-    let borrar = contactos.includes(borrar);
-    if (contactos[0].id == borrar){
-        let posicion = contactos.indexOf(borrar);
-        contactos.splice(posicion,1);
-        return contactos
-    }
+    let posicion = false;
+    let index = null;
+    for (let i = 0; i < contactos.length; i++ ){
+        if (contactos[i].id == id){
+            posicion = true;
+            index = i;          
+    }  
+    }    
+    if (posicion == true){
+        alert("EL contacto con la ID indicada ya se encuentra registrado." )          
+    } 
     else{
-        alert("EL contacto " + borrarContacto + " NO existe." )
-    }      
+        contactos.push(contacto3);
+        alert("Se agrega contacto con la ID:  " + id )   
+    }            
 }
+ 
 
+function eliminarContacto(){ 
+    let posicion = false;
+    let index = null;
+    for (let i = 0; i < contactos.length; i++ ){
+        if (contactos[i].id == borrar){
+            posicion = true;
+            index = i;          
+    }  
+    }    
+    if (posicion == true){
+        contactos.splice(index,1);
+        alert("Se elimina el contacto con la ID: " + borrar )           
+    } 
+    else{
+        alert("EL contacto a eliminar con la ID indicada NO existe." )
+    }        
+              
+}
 
 function imprimirConsola(){ 
     agregarContacto();
-    //eliminarContacto();
+    eliminarContacto();
     return contactos;     
 }
 
